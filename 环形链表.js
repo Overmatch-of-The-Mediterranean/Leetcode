9ce3bb78c2d60ac0c13538260affc35609ab1028
地中海强者 <101050485+Overmatch-of-The-Mediterranean@users.noreply.github.com>
2022-08-20 17:47:11 +0800

    第一题

--- 环形链表.js
+++ 环形链表.js
@@ -13,3 +13,17 @@ var hasCycle = function (head) {
   // 若走到头，即，next===null，就是无环
   return false
 };
+
+// 第二种,双指针,效率高,学的网上的
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow == fast) {
+      return true
+    }
+  }
+  return false
+};
