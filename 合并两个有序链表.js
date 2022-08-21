ebe3fcd24733f66765f69baba1121fe9be042001
地中海强者 <101050485+Overmatch-of-The-Mediterranean@users.noreply.github.com>
2022-08-21 20:36:07 +0800

    第二题

--- 合并两个有序链表.js
+++ 合并两个有序链表.js
@@ -1,18 +1,23 @@
-// 建立虚拟头节点
+// 建立虚拟列表
 var mergeTwoLists = function (list1, list2) {
   // 虚拟头节点每次比较后，指向小的节点，谁小，谁所在链表的指向下个节点，继续和大的节点比较。
-  let head = new NodeList(-1);
-  let p1 = list1
+  let head = new ListNode();
+  let p1 = list1;
   let p2 = list2;
+  let p = head;
   while (p1 && p2) {
     if (p1.val < p2.val) {
-      head.next = p1;
+      p.next = p1;
       p1 = p1.next
     } else {
-      head.next = p2;
+      p.next = p2;
       p2 = p2.next;
     }
+    p = p.next;
   }
+  if (p1) p.next = p1;
+  if (p2) p.next = p2;
+  return head.next;
 }
 
 
