d17049cb85781cb56f5621dcad6e7ad61c31b651
地中海强者 <101050485+Overmatch-of-The-Mediterranean@users.noreply.github.com>
2022-08-25 21:17:43 +0800

    yahu~

--- 160. 相交链表.js
+++ 160. 相交链表.js
@@ -3,7 +3,7 @@
 var getIntersectionNode = function (headA, headB) {
   let p1 = headA;
   let p2 = headB;
-  if (!p1 || !p2) return null; //不多余，会让代码执行次数更少
+  if (!p1 || !p2) return null;  // 不多余，会让代码执行次数更少，如，对于[2,4,5,6,7]和[],可直接返回。如果没有这一步，会执行下列循环，直至p1==p2==null,也能得到正确结果，但代码执行次数增加了。
   while (p1 !== p2) {
     p1 = p1 == null ? headB : p1.next;
     p2 = p2 == null ? headA : p2.next;
