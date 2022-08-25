b509b778ac77fc41035bf02dca0a674cfc09dd80
地中海强者 <101050485+Overmatch-of-The-Mediterranean@users.noreply.github.com>
2022-08-25 10:46:25 +0800

    第四题

---
+++ 160. 相交链表.js
@@ -0,0 +1,31 @@
+// 双指针
+// 要点：消除长度差异
+var getIntersectionNode = function (headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
+  if (!p1 || !p2) return null;
+  while (p1 !== p2) {
+    p1 = p1 == null ? headB : p1.next;
+    p2 = p2 == null ? headA : p2.next;
+  }
+  return p1;
+};
+
+// 哈希表
+var getIntersectionNode = function (headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
+  if (!p1 || !p2) return null;
+  const map = new Map();
+  while (p1) {
+    map.set(p1);
+    p1 = p1.next;
+  }
+  while (p2) {
+    if (map.has(p2)) {
+      return p2;
+    } else {
+      p2 = p2.next
+    }
+  }
+};
